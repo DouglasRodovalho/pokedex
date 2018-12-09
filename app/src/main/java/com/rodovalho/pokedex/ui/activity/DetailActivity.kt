b6d5d4c503e1e86ce0ad1885d1b62f255355b802
Douglas Rodovalho <jesusrodovalho.douglas@gmail.com>
2018-12-09 15:25:54 -0200

    Definição de estrutura de arquivos, inclusão de dependencias, layout basico, e liberações necessárias

--- app/src/main/java/com/rodovalho/pokedex/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/rodovalho/pokedex/ui/activity/DetailActivity.kt
@@ -1,12 +1,13 @@
-package com.rodovalho.pokedex
+package com.rodovalho.pokedex.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.rodovalho.pokedex.R
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
