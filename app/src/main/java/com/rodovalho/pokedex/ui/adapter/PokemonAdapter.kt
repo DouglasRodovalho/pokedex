ebd56292144547554096c1a0b8e2194880f630ec
Douglas Rodovalho <dr803950@hpeautos.com.br>
2018-12-14 07:50:14 -0200

    criação da view model

---
+++ app/src/main/java/com/rodovalho/pokedex/ui/adapter/PokemonAdapter.kt
@@ -0,0 +1,4 @@
+package com.rodovalho.pokedex.ui.adapter
+
+class PokemonAdapter {
+}
---
+++ app/src/main/java/com/rodovalho/pokedex/ui/util/PokemonViewModel.kt
@@ -0,0 +1,27 @@
+package com.rodovalho.pokedex.ui.util
+
+import android.arch.lifecycle.ViewModel
+import com.rodovalho.pokedex.data.PokemonDataSource
+import com.rodovalho.pokedex.model.Pokemon
+import rx.Observable
+
+class PokemonViewModel : ViewModel() {
+    private var pokemons = mutableListOf<Pokemon>()
+
+    var isLoading = false
+        private set
+
+    var currentPage = -1
+        private set
+
+    val limit = 20
+
+    fun load(page: Int) : Observable<Pokemon> {
+        return if (page <= currentPage) {
+            Observable.from(pokemons)
+        } else {
+            currentPage = page
+            PokemonDataSource().loadPokemons(limit, page)
+        }
+    }
+}
